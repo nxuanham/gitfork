57f37ad587f6d369dc91328d515baad3e5bfc509
Xuan Ham <LFTV_LT_003@LFTV-LT-003.local>
2021-01-26 14:58:39 +0700

    Modidy test 2 Add test 3

--- test2.php
+++ test2.php
@@ -4,4 +4,12 @@ $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
 foreach($age as $x => $val) {
   echo "$x = $val<br>";
 }
+
+define("cars", [
+  "Alfa Romeo",
+  "BMW",
+  "Toyota"
+]);
+echo cars[0];
+
 ?>
